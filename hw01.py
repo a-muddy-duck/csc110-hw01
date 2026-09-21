ac146d2efbcb79e4b5e8ed308b8176125d786603
a-muddy-duck <arulifson@smith.edu>
2026-09-21 09:47:43 -0400

    indented code to be in main, works now

--- hw01.py
+++ hw01.py
@@ -46,64 +46,64 @@ def main():
     # Part 1: Basic Operations
     # =============================================
     # Your code for part 1 under this line and before the print statements
-# reassign variables
-x = 27
-y = 1
-a=1.5
-b=7
-c=-1
+    # reassign variables
+    x = 27
+    y = 1
+    a=1.5
+    b=7
+    c=-1
 
-# write math for result1
-result1 = ((3*x) - (9*y))/((2*a)*(b-c))
+    # write math for result1
+    result1 = ((3*x) - (9*y))/((2*a)*(b-c))
 
-# print results
-for loopvariable in[f"x = {x}", f"y = {y}", f"a = {a}", f"b = {b}", f"c = {c}", f"result = {result1}"]:
-    print("Part 1:", loopvariable)
-    
-    # End of Part 1 ---------------------- 
-
-
-    # Part 2: Power
-    # =============================================
-    # Your code for part 2 under this line and before the print statements
-# overwrite variables
-x =5
-y= -3
-
-# do math
-result2 = x**2*y**4
-
-#print results
-print("Part 2: x =", x)
-print("Part 2: y =", y)
-print("Part 2: result =", result2)
-    # End of Part 2 ---------------------- don't forget to add and commit after part 2
+    # print results
+    for loopvariable in[f"x = {x}", f"y = {y}", f"a = {a}", f"b = {b}", f"c = {c}", f"result = {result1}"]:
+        print("Part 1:", loopvariable)
+        
+        # End of Part 1 ---------------------- 
 
 
+        # Part 2: Power
+        # =============================================
+        # Your code for part 2 under this line and before the print statements
+    # overwrite variables
+    x =5
+    y= -3
 
-    # Part 3: Integer divide
-    # =============================================
-    # Your code for part 3 under this line and before the print statements
-# overwrite variables
-a = 100 #treats
-b=13 #dogs
+    # do math
+    result2 = x**2*y**4
 
-#math & print
-result3 = a//b
-print("Part 3: a =",a)
-print("Part 3: b =",b)
-print("Part 3: result =",result3)
-    # End of Part 3 ----------------------
+    #print results
+    print("Part 2: x =", x)
+    print("Part 2: y =", y)
+    print("Part 2: result =", result2)
+        # End of Part 2 ---------------------- don't forget to add and commit after part 2
 
 
-    # Part 4: Modulo
-    # =============================================
-    # Your code for part 4 under this line and before the print statements
-#math & print
-result4 = a%b
 
-print("Part 4: result =", result4)
-    # End of Part 4 ----------------------
+        # Part 3: Integer divide
+        # =============================================
+        # Your code for part 3 under this line and before the print statements
+    # overwrite variables
+    a = 100 #treats
+    b=13 #dogs
+
+    #math & print
+    result3 = a//b
+    print("Part 3: a =",a)
+    print("Part 3: b =",b)
+    print("Part 3: result =",result3)
+        # End of Part 3 ----------------------
+
+
+        # Part 4: Modulo
+        # =============================================
+        # Your code for part 4 under this line and before the print statements
+    #math & print
+    result4 = a%b
+
+    print("Part 4: result =", result4)
+        # End of Part 4 ----------------------
 
 if __name__ == "__main__":
     main()
